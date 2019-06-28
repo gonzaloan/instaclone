74a8c282b48a6bce2b0944ac0eed13240f6bb57f
Gonzalo Muñoz <gonzalo.munoz.ext@sodexo.com>
2019-06-28 10:47:48 -0400

    Add logout y signup

--- users/views.py
+++ users/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
 
+# Exceptions
+from django.db.utils import IntegrityError
 def login_view(request):
     """Login view"""
     if request.method == 'POST':
@@ -28,4 +33,30 @@ def logout_view(request):
     return redirect('login')
 
 
+def signup_view(request):
+    """Signup View"""
+    if request.method == 'POST':
+        username=request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation doesnt match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'User already taken'})
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.save()
+        # Crear un profile
+        profile = Profile(user= user)
+        profile.save()
+
+        return redirect('login')
+    return render(request, "users/signup.html")
 
